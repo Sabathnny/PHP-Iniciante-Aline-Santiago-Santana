7bb40d382e3faf3ba62d667b618e351f8f19508c
Sabathnny <70477929+Sabathnny@users.noreply.github.com>
2020-11-18 22:51:05 -0300

    Initial commit

---
+++ Pedido_de_Venda.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.1.0-dev
+ */
+
+/**
+ * Database `ALINESANTIAGOSANTANA`
+ */
+
+/* `ALINESANTIAGOSANTANA`.`Pedido de Venda` */
+$Pedido_de_Venda = array(
+);
---
+++ Produtos_Loja_Aline_Eletro.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.1.0-dev
+ */
+
+/**
+ * Database `ALINESANTIAGOSANTANA`
+ */
+
+/* `ALINESANTIAGOSANTANA`.`Produtos Loja Aline Eletro` */
+$Produtos_Loja_Aline_Eletro = array(
+);
